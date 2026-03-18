4726f2156bc81aad552170048693099857358344
yooseungyeon31 <ysy8317433@gmail.com>
2026-03-18 11:04:09 +0900

    사과 연상(수업 빌드업)

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+//-----------------사과를 연상하며-------------
+////Memory
+//class Apple
+//{
+//public:
+//	int Color = 1; //0: 노란색, 1: 빨간색
+//	int Shape = 3;//0:네모, 1: 세모, 3: 동그라미
+//	int 꼭지 = 1; //갯수 한 개
+//	bool InTree = true;
+//	int Delicious = 2;
+//
+//
+//
+//
+//	void EatBy(int who);
+//	void GrowUp();
+//	void Fall();
+//
+//
+//};
+//
+//void Apple::EatBy(int who)
+//{
+//}
+//
+//void Apple::GrowUp()
+//{
+//
+//}
+//
+//void Apple::Fall()
+//{
+//}
+//
+//
+//
+//
+//int main()
+//{
+//	Apple MyApple;
+//
+//	MyApple.GrowUp();
+//
+//
+//	return 0;
+//}
+//
+//
+//
+
